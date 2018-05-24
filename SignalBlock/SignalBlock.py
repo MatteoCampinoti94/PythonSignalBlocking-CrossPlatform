bb102027f0b7606af6dcc2bd72aa777fd401d632
MatteoCampinoti94 <matteo.campinoti94@gmail.com>
2018-05-24 18:18:14 +0200

    Add signal unblocking with original handler restoration

--- SignalBlock/SignalBlock.py
+++ SignalBlock/SignalBlock.py
@@ -6,15 +6,37 @@ def signal_handler(signum, frame):
 def signal_clear():
     sigblock._pending = ()
 
+def blocked_clear(sig=None):
+    if sig == None:
+        sigblock._blocked = {}
+    else:
+        del sigblock._blocked[sig]
+
 class sigblock:
     _pending = ()
     _handler = signal_handler
+    _blocked = {}
 
     def block(sig, handler=_handler):
         if type(sig) != signal.Signals:
             raise TypeError('Argument has to be of type signal.Signals')
 
+        if sig not in sigblock._blocked:
+            sigblock._blocked[sig] = signal.getsignal(sig)
         signal.signal(sig, handler)
+
+    def unblock(sig=None):
+        if sig == None:
+            for sig in sigblock._blocked:
+                signal.signal(sig, sigblock._blocked[sig])
+        else:
+            if type(sig) != signal.Signals:
+                raise TypeError('Argument has to be of type signal.Signals')
+            elif sig not in sigblock._blocked:
+                return
+
+            signal.signal(sig, sigblock._blocked[sig])
+            blocked_clear(sig)
 
     def pending(sig=None):
         if sig != None:
